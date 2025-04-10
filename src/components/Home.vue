4a9c41a86ec721758c60186b62eb269b798a9088
Jessica <jessica.santosb@outlook.com>
2025-04-10 09:48:27 -0300

    feat: add filters

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,50 @@
 <template>
   <main class="min-vh-100 container">
+    <div class="filter w-100 d-flex my-4">
+      <input
+        type="text"
+        ref="queryName"
+        class="w-75 p-2"
+        placeholder="filtre por nome"
+      />
+      <button type="button" v-on:click="filterPokemonByName()" class="w-25">
+        Procurar
+      </button>
+    </div>
+    <div class="filter w-100 d-flex my-4">
+      <input
+        type="text"
+        ref="queryId"
+        class="w-75 p-2"
+        placeholder="filtre por ID"
+      />
+      <button type="button" v-on:click="filterPokemonById()" class="w-25">
+        Procurar
+      </button>
+    </div>
+    <div class="filter w-100 d-flex my-4">
+      <input
+        type="text"
+        ref="queryType"
+        class="w-75 p-2"
+        placeholder="filtre por tipo"
+      />
+      <button type="button" v-on:click="filterPokemonByType()" class="w-25">
+        Procurar
+      </button>
+    </div>
+    <div class="filter w-100 d-flex my-4">
+      <input
+        type="text"
+        ref="querySpecie"
+        class="w-75 p-2"
+        placeholder="filtre por espécie"
+      />
+      <button type="button" v-on:click="filterPokemonBySpecie()" class="w-25">
+        Procurar
+      </button>
+    </div>
+
     <ul class="row">
       <pokemon-card
         v-for="pokemon in pokemons"
@@ -30,6 +75,7 @@ import axios from "axios";
 
 export default {
   name: "HomePage",
+  el: ".root",
   data() {
     return {
       pokemons: [],
@@ -83,6 +129,51 @@ export default {
       this.selectedPokemon = null;
 
       console.log("close", this.showModal);
+    },
+
+    // filtros para nome, ID, tipo e espécie
+    filterPokemonByName() {
+      const query = this.$refs.queryName.value.toLowerCase();
+      const allPokemon = this.pokemons;
+
+      const queryByName = allPokemon.filter(({ name }) =>
+        name.toLowerCase().includes(query)
+      );
+
+      this.pokemons = queryByName;
+    },
+
+    filterPokemonById() {
+      const query = this.$refs.queryId.value.toLowerCase();
+      const allPokemon = this.pokemons;
+
+      const queryById = allPokemon.filter(({ id }) => id.toString() === query);
+
+      this.pokemons = queryById;
+    },
+
+    filterPokemonByType() {
+      const query = this.$refs.queryType.value.toLowerCase();
+      const allPokemon = this.pokemons;
+
+      const queryByType = allPokemon.filter(({ types }) => {
+        for (const type of types) {
+          return type.type.name.toLowerCase().includes(query);
+        }
+      });
+
+      this.pokemons = queryByType;
+    },
+
+    filterPokemonBySpecie() {
+      const query = this.$refs.querySpecie.value.toLowerCase();
+      const allPokemon = this.pokemons;
+
+      const queryBySpecies = allPokemon.filter(({ species }) =>
+        species.name.includes(query)
+      );
+
+      this.pokemons = queryBySpecies;
     },
   },
 };
